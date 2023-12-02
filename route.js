52239e0993da2e21a1eb07d905f3ce75d8044631
KrystianGH2 <kgcdronio@gmail.com>
2023-12-02 01:59:22 +0100

    navigation

---
+++ route.js
@@ -0,0 +1,7 @@
+export const navigation = [
+    { name: "Home", href: "/home" },
+    { name: "Auctions", href: "/auctions" },
+    { name: "About", href: "/about" },
+    { name: "Contact", href: "/contact" },
+    [{ name: "login", href: "/login" }],
+  ];
