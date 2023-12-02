5cf5bf861d7c5ee079dc96a2b2d3fbafcbe1e8fc
KrystianGH2 <kgcdronio@gmail.com>
2023-12-02 02:03:00 +0100

    footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,125 @@
+// import React from 'react'
+import bidBuddies from "../../assets/images/bidBuddies.png";
+
+export default function Footer() {
+  return (
+    <div className="">
+      <div>
+        <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8 max-w-7xl mx-auto" />
+      </div>
+      <div className="">
+        <footer className="bg-white">
+          <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+            <div className="md:flex md:justify-between">
+              <div className="mb-6 md:mb-0">
+                <a href="#" className="flex items-center">
+                  <img
+                    src={bidBuddies}
+                    className="h-[80px]"
+                    alt="FlowBite Logo"
+                  />
+                </a>
+              </div>
+              <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+                <div>
+                  <h2 className="mb-6 text-md font-semibold text-black uppercase">
+                    Resources
+                  </h2>
+                  <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                    <li className="mb-4">
+                      <a
+                        href="https://flowbite.com/"
+                        className="hover:underline"
+                      >
+                        Flowbite
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://tailwindcss.com/"
+                        className="hover:underline"
+                      >
+                        Tailwind CSS
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <div>
+                  <h2 className="mb-6 text-md font-semibold text-black uppercase">
+                    Follow us
+                  </h2>
+                  <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                    <li className="mb-4">
+                      <a
+                        href="https://github.com/themesberg/flowbite"
+                        className="hover:underline "
+                      >
+                        Github
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://discord.gg/4eeurUVvTy"
+                        className="hover:underline"
+                      >
+                        Discord
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <div>
+                  <h2 className="mb-6 text-md font-semibold text-black uppercase">
+                    Legal
+                  </h2>
+                  <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                    <li className="mb-4">
+                      <a href="#" className="hover:underline">
+                        Privacy Policy
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="hover:underline">
+                        Terms &amp; Conditions
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="sm:flex sm:items-center sm:justify-between">
+              <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+                © 2023{" "}
+                <a href="https://flowbite.com/" className="hover:underline">
+                  Flowbite™
+                </a>
+                . All Rights Reserved.
+              </span>
+              <div className="flex mt-4 sm:justify-center sm:mt-0">
+                <a
+                  href="#"
+                  className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+                >
+                  <svg
+                    className="w-4 h-4"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 8 19"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <span className="sr-only">Facebook page</span>
+                </a>
+                {/* ... (similar code for other social media icons) ... */}
+              </div>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>
+  );
+}
