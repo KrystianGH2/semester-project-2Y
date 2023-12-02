89e8363b27283b9f2e0c96f010466a241f209bb8
KrystianGH2 <kgcdronio@gmail.com>
2023-12-02 02:15:08 +0100

    changed navigateToHom from /home to just /

--- src/components/Navigation/Login/Login.jsx
+++ src/components/Navigation/Login/Login.jsx
@@ -14,7 +14,7 @@ function LoginLogic() {
   // This navigates to the home page after a successful login
   const navigateToHome = () => {
     setTimeout(() => {
-      navigate("/home");
+      navigate("/");
       window.location.reload();
     }, 2000);
   };
