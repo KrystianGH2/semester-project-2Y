f38dc2ad81f0ba61e26be13500b8d4fe2f97127c
KrystianGH2 <kgcdronio@gmail.com>
2023-12-11 22:15:00 +0100

    Fixed hover button color

--- src/components/Auctions/Auctions.jsx
+++ src/components/Auctions/Auctions.jsx
@@ -242,7 +242,7 @@ function Listings() {
                   <button
                     disabled
                     type="button"
-                    className="text-white bg-orange-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center"
+                    className="text-white bg-orange-500 hover:bg-orange-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2  inline-flex items-center"
                   >
                     <svg
                       aria-hidden="true"
@@ -268,7 +268,7 @@ function Listings() {
                 <div className="flex items-center justify-center">
                   <button
                     onClick={handleLoadMore}
-                    className="mt-8 bg-blue-500 text-white px-4 py-2 rounded items-center justify-center hover:bg-orange-500"
+                    className="mt-8 bg-orange-500 text-white px-4 py-2 rounded items-center justify-center hover:bg-orange-400 shadow-lg transition-all"
                   >
                     Load More
                   </button>
