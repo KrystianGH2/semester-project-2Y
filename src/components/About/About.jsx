58fac378c10b0b9a7a4b6ef37d81c90ecb78e62d
KrystianGH2 <kgcdronio@gmail.com>
2023-12-11 22:44:29 +0100

    About and contact has no content - no purpose

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 function About() {
   return (
-    <div>
-      <h1>About</h1>
+    <div className=" flex flex-col h-[60vh] justify-center items-center ">
+      <h1 className="tracking-widest font-extrabold text-5xl">COMING SOON</h1>
     </div>
   );
 }
