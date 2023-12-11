30d664260c54d67c4229aea8dbc2a7b25459a8dd
KrystianGH2 <kgcdronio@gmail.com>
2023-12-11 19:32:14 +0100

    created a loading state while fetching

--- src/components/Navigation/Home/index.jsx
+++ src/components/Navigation/Home/index.jsx
@@ -1,10 +1,62 @@
+import { useState, useEffect } from "react";
+import HomeUi from "./homeUi";
+import SkeletonMain from "../../skeleton/skeletonMain";
 
-function Home() {
+const Home = () => {
+  const [latestListings, setLatestListings] = useState([]);
+  const [mostLikedListings, setMostLikedListings] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const url =
+    "https://api.noroff.dev/api/v1/auction/listings?_active=true&_seller=true&_bids=true";
+
+  useEffect(() => {
+    const getListings = async () => {
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+
+        if (res.ok) {
+          const sortedListingsLatest = [...data].sort(
+            (a, b) => new Date(b.created) - new Date(a.created)
+          );
+          const sortedListingsMostLiked = [...data].sort(
+            (a, b) => b._count.bids - a._count.bids
+          );
+
+          setLatestListings(sortedListingsLatest.slice(0, 6));
+          setMostLikedListings(sortedListingsMostLiked.slice(0, 6));
+        }
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getListings();
+  }, [url]);
+
+  const formatDate = (dateString) => {
+    const options = { year: "numeric", month: "short", day: "numeric" };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
+
   return (
-    <div>
-     <h1>HomePage</h1>
-    </div>
-  )
-}
+    <>
+      {loading ? (
+        // While loading, render SkeletonMain with 6 cards
+        <SkeletonMain count={6} />
+      ) : (
+        // After fetching data, render HomeUi
+        <HomeUi
+          latestListings={latestListings}
+          mostLikedListings={mostLikedListings}
+          formatDate={formatDate}
+        />
+      )}
+    </>
+  );
+};
 
-export default Home
+export default Home;
