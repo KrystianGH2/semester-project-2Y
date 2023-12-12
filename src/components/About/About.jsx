c8a952e94def1fd5a098592786b39143ef14f406
KrystianGH2 <kgcdronio@gmail.com>
2023-12-13 00:00:06 +0100

    adjusted view height

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import funnyCaveMan from "../../assets/images/funnyCaveMan.gif";
 function About() {
   return (
-    <div className=" flex flex-col h-[60vh] justify-center items-center ">
+    <div className=" flex flex-col h-[50vh]  sm:h-[70vh] justify-center items-center ">
       <h1 className="tracking-widest font-extrabold text-5xl">COMING SOON</h1>
       <img src={funnyCaveMan} alt="Funny caveman animation" />
     </div>
