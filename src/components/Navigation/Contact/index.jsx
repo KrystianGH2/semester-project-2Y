5be3d66d61d96e36496095779b403338c067d952
KrystianGH2 <kgcdronio@gmail.com>
2023-12-12 23:58:04 +0100

    Added funnyCaveMan gif on about and contact page to not leave it and empty space

--- src/components/Navigation/Contact/index.jsx
+++ src/components/Navigation/Contact/index.jsx
@@ -1,7 +1,9 @@
+import funnyCaveMan from "../../../assets/images/funnyCaveMan.gif";
 function Contact() {
   return (
     <div className=" flex flex-col h-[60vh] justify-center items-center ">
       <h1 className="tracking-widest font-extrabold text-5xl">COMING SOON</h1>
+      <img src={funnyCaveMan} alt="Funny caveman animation" />
     </div>
   );
 }
