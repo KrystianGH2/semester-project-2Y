9fe83dccf3dccc537e44b0c7f8b00fed55419ebc
KrystianGH2 <kgcdronio@gmail.com>
2023-12-13 23:46:09 +0100

    Changed the color of the submit button on create listings

--- src/components/Create/createListingUi.jsx
+++ src/components/Create/createListingUi.jsx
@@ -113,7 +113,7 @@ const CreateUI = ({
 
           <button
             type="submit"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="text-white bg-orange-500 hover:bg-orange-400 transition hover:shadow-lg focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center "
           >
             Submit
           </button>
@@ -200,7 +200,7 @@ const CreateUI = ({
             <div className="flex items-center justify-center">
               <button
                 onClick={handleLoadMore}
-                className="mt-8 bg-orange-500 text-white px-4 py-2 rounded items-center justify-center hover:bg-[#FF8C00]"
+                className="mt-8 bg-orange-500 text-white px-4 py-2 rounded items-center justify-center hover:bg-orange-400 hover:shadow-lg transition-all"
               >
                 Load More
               </button>
