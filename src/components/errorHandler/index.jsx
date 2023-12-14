86b0a72b422ac9c2e09422f344b71a832342aadf
KrystianGH2 <kgcdronio@gmail.com>
2023-12-14 21:46:16 +0100

    created errorHandling to render when response is not ok

---
+++ src/components/errorHandler/index.jsx
@@ -0,0 +1,9 @@
+function ErrorHandler() {
+  return (
+    <div className="mx-auto max-w-3xl px-6 lg:px-8 mt-36 border-2 w-full text-red-600 border-red-600 rounded-md flex justify-center items-center text-center font-bold tracking-wider text-lg h-full my-10 p-6">
+      <h1>Error! Failed fetching data: </h1>
+    </div>
+  );
+}
+
+export default ErrorHandler;
